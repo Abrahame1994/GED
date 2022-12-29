d9a503b9f75a50797d08e1fc16a45c314aa1b100
Abrahame1994 <soropoguidoboabraham@gmail.com>
2022-12-29 06:00:49 +0000

    Initialisation du mise a jour du fichier setting.py

--- Docubase/Docubase/settings.py
+++ Docubase/Docubase/settings.py
@@ -28,15 +28,14 @@ SECRET_KEY = 'django-insecure-ee*x9i$j*^pgtyg@hycplig_e)==q-=3cm1)sil)6y=9t7(la1
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '192.168.82.2',
-    '192.168.82.186',
-    '192.168.82.183',
-    '192.168.82.176',
-    "192.168.43.240",
-    "10.0.180.250",
-    "10.0.180.37"
-    
-
+    # '192.168.82.2',
+    # '192.168.82.186',
+    # '192.168.82.183',
+    # '192.168.82.176',
+    # "192.168.43.240",
+    # "10.0.180.250",
+    # "10.0.180.37",
+    "127.0.0.1"
 ]
 
 
@@ -89,24 +88,24 @@ WSGI_APPLICATION = 'Docubase.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'mysql.connector.django',
-        #'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'balta',
-        'USER': 'baltazar',
-        'PASSWORD': 'baltazar',
-        'HOST': '192.168.82.2',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'mysql.connector.django',
+#         #'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'balta',
+#         'USER': 'baltazar',
+#         'PASSWORD': 'baltazar',
+#         'HOST': '192.168.82.2',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
+# }
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
